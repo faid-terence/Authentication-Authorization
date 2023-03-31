028e5c486c3479493772329ed4f4d3241b5b712e
Faid Terence JABO <faidterence@outlook.com>
2023-03-31 17:57:43 +0200

    Updated Packages

--- app.js
+++ app.js
@@ -25,3 +25,9 @@ app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+
+
+app.listen(4000, () => {
+  console.log('Server started on port 4000');
+});
--- controllers/authController.js
+++ controllers/authController.js
@@ -57,6 +57,7 @@ module.exports.signup_post = async (req, res) => {
 
   try {
     const user = await User.create({ email, password });
+    
     const token = createToken(user._id);
     res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(201).json({ user: user._id });
